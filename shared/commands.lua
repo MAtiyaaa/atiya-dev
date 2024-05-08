f81c53c4c7bd167fde138cfec075d130028108bc
Atiya <matiyaaaa@gmail.com>
2024-05-08 23:30:35 +0400

    Update and rename config.lua to commands.lua

---
+++ shared/commands.lua
@@ -0,0 +1,158 @@
+AD = {}
+AD.Commands = {
+    isadmin = {
+        name = "isadmin",
+    },
+    jobname = {
+        name = "jobname",
+    },
+    jobtype = {
+        name = "jobtype",
+    },
+    who = {
+        name = "who",
+    },
+    iteminfo = {
+        name = "iteminfo",
+    },
+    jail = {
+        name = "jaila",
+    },
+    unjail = {
+        name = "unjaila",
+    },
+    god = {
+        name = "god",
+    },
+    invincibility = {
+        name = "invincibility",
+    },
+    invisibility = {
+        name = "invisibility",
+    },
+    ammo = {
+        name = "ammo",
+    },
+    freeze = {
+        name = "freeze",
+    },
+    unfreeze = {
+        name = "unfreeze",
+    },
+    spawnobject = {
+        name = "spawnobject",
+    },
+    deleteobject = {
+        name = "deleteobject",
+    },
+    deleteobjectsinradius = {
+        name = "deleteobjectsinradius",
+    },
+    delvehicle = {
+        name = "delvehicle",
+    },
+    delvehicleinradius = {
+        name = "delvehicleinradius",
+    },
+    repairvehicle = {
+        name = "repairvehicle",
+    },
+    coords3 = {
+        name = "coords3",
+    },
+    coords4 = {
+        name = "coords4",
+    },
+    togglecoords = {
+        name = "togglecoords",
+    },
+    livemarker = {
+        name = "livemarker",
+    },
+    setstress = {
+        name = "setstress",
+    },
+    getidentifier = {
+        name = "getidentifier",
+    },
+    startpolyzone = {
+        name = "startpolyzone",
+    },
+    addpolyzonepoint = {
+        name = "addpolyzonepoint",
+    },
+    finishpolyzone = {
+        name = "finishpolyzone",
+    },
+    showprops = {
+        name = "showprops",
+    },
+    showpeds = {
+        name = "showpeds",
+    },
+    activatelaser = {
+        name = "activatelaser",
+    },
+    toggledevinfo = {
+        name = "toggledevinfo",
+    },
+    showcarinfo = {
+        name = "showcarinfo",
+    },
+    applyeffect = {
+        name = "applyeffect",
+    },
+    livery = {
+        name = "livery",
+    },
+    vehiclespeed = {
+        name = "vehiclespeed",
+    },
+    pedspeed = {
+        name = "pedspeed", -- don't work :(
+    },
+    spawnped = {
+        name = "spawnped",
+    },
+    spawnpedcoords = {
+        name = "spawnpedcoords",
+    },
+    toggleped = {
+        name = "toggleped",
+    },
+    resetped = {
+        name = "resetped",
+    },
+    clearnearbypeds = {
+        name = "clearnearbypeds",
+    },
+    clearpedsradius = {
+        name = "clearpedsradius",
+    },
+    noclip = {
+        name = "noclipa",
+    },
+    settime = {
+        name = "settime",
+    },
+    setweather = {
+        name = "setweather",
+    },
+    addAttachments = {
+        name = "addweaponattachments",
+    },
+    sethealth = {
+        name = "sethealth",
+    },
+    setarmor = {
+        name = "setarmor",
+    },
+    die = {
+        name = "die",
+    },
+    handcuff = {
+        name = "handcuff",
+    },
+}
+
+return AD
