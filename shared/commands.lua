4b290af1636cb59bdcf478022af6c1d85955fc02
Atiya <matiyaaaa@gmail.com>
2024-05-10 03:16:42 +0400

    Update commands.lua

--- shared/commands.lua
+++ shared/commands.lua
@@ -1,168 +1,232 @@
 AD = {}
 AD.Commands = {
+        enabled = true,
     isadmin = {
+        enabled = true,
         name = "isadmin",
     },
     jobname = {
+        enabled = true,
         name = "jobname",
     },
     jobtype = {
+        enabled = true,
         name = "jobtype",
     },
     who = {
+        enabled = true,
         name = "who",
     },
     iteminfo = {
+        enabled = true,
         name = "iteminfo",
     },
     jail = {
+        enabled = true,
         name = "jaila",
     },
     unjail = {
+        enabled = true,
         name = "unjaila",
     },
     god = {
+        enabled = true,
         name = "god",
     },
     invincibility = {
+        enabled = true,
         name = "invincibility",
     },
     invisibility = {
+        enabled = true,
         name = "invisibility",
     },
     ammo = {
+        enabled = true,
         name = "ammo",
     },
     freeze = {
+        enabled = true,
         name = "freeze",
     },
     unfreeze = {
+        enabled = true,
         name = "unfreeze",
     },
     spawnobject = {
+        enabled = true,
         name = "spawnobject",
     },
     deleteobject = {
+        enabled = true,
         name = "deleteobject",
     },
     deleteobjectsinradius = {
+        enabled = true,
         name = "deleteobjectsinradius",
     },
     startobjectplace = {
+        enabled = true,
         name = "liveobj",
     },
     deleteliveobj = {
+        enabled = true,
         name = "liveobjd",
     },
     livemarker = {
+        enabled = true,
         name = "livemarker",
     },
     coords3 = {
+        enabled = true,
         name = "coords3",
     },
     coords4 = {
+        enabled = true,
         name = "coords4",
     },
     togglecoords = {
+        enabled = true,
         name = "togglecoords",
     },
     getidentifier = {
+        enabled = true,
         name = "getidentifier",
     },
     startpolyzone = {
+        enabled = true,
         name = "startpolyzone",
     },
     addpolyzonepoint = {
+        enabled = true,
         name = "addpolyzonepoint",
     },
     finishpolyzone = {
+        enabled = true,
         name = "finishpolyzone",
     },
     showprops = {
+        enabled = true,
         name = "showprops",
     },
     showpeds = {
+        enabled = true,
         name = "showpeds",
     },
     activatelaser = {
+        enabled = true,
         name = "activatelaser",
     },
     toggledevinfo = {
+        enabled = true,
         name = "toggledevinfo",
     },
     showcarinfo = {
+        enabled = true,
         name = "showcarinfo",
     },
     delvehicle = {
+        enabled = true,
         name = "delvehicle",
     },
     delvehicleinradius = {
+        enabled = true,
         name = "delvehicleinradius",
     },
     repairvehicle = {
+        enabled = true,
         name = "repairvehicle",
     },
     livery = {
+        enabled = true,
         name = "livery",
     },
     vehiclespeed = {
+        enabled = true,
         name = "vehiclespeed",
     },
     pedspeed = {
-        name = "pedspeed", -- don't work :(
+        enabled = true,
+        name = "pedspeed", -- don't work :( lol i gotta set it to 1.49 ill do it l8r
     },
     spawnped = {
+        enabled = true,
         name = "spawnped",
     },
     spawnpedcoords = {
+        enabled = true,
         name = "spawnpedcoords",
     },
+    liveped = {
+        enabled = true,
+        name = "liveped",
+    },
+    deleteliveped = {
+        enabled = true,
+        name = "livepedd",
+    },
     toggleped = {
+        enabled = true,
         name = "toggleped",
     },
     resetped = {
+        enabled = true,
         name = "resetped",
     },
     clearnearbypeds = {
+        enabled = true,
         name = "clearnearbypeds",
     },
     clearpedsradius = {
+        enabled = true,
         name = "clearpedsradius",
     },
     noclip = {
+        enabled = true,
         name = "noclipa",
     },
     settime = {
+        enabled = true,
         name = "settime",
     },
     setweather = {
+        enabled = true,
         name = "setweather",
     },
     addAttachments = {
+        enabled = true,
         name = "addweaponattachments",
     },
     sethealth = {
+        enabled = true,
         name = "sethealth",
     },
     setarmor = {
+        enabled = true,
         name = "setarmor",
     },
     setstress = {
+        enabled = true,
         name = "setstress",
     },
     sethunger = {
+        enabled = true,
         name = "sethunger",
     },
     setthirst = {
+        enabled = true,
         name = "setthirst",
     },
     applyeffect = {
+        enabled = true,
         name = "applyeffect",
     },
     die = {
+        enabled = true,
         name = "die",
     },
     handcuff = {
+        enabled = true,
         name = "handcuff",
     },
 }
